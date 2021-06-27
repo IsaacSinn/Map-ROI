abfb9e6772bc314da0662ce8cfe2a1ceffa52f39
IsaacSin <43310667+IsaacSinn@users.noreply.github.com>
2021-06-28 00:01:43 +0800

    update LineDetection.py

--- LineDetection.py
+++ LineDetection.py
@@ -1,34 +1,84 @@
-import cv2
+import cv2 as cv
 import numpy as np
 
-img = cv2.imread('CoralReef1.jpg')
-gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+img = cv.imread('CoralReef1.jpg')
+gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
 
-kernel_size = 5
-blur_gray = cv2.GaussianBlur(gray,(kernel_size, kernel_size),0)
+def emptyFunction():
+    pass
 
-low_threshold = 50
-high_threshold = 150
-edges = cv2.Canny(blur_gray, low_threshold, high_threshold)
+#color palette
+LowImageBlue = np.zeros((512, 512, 3), np.uint8)
+HighImageBlue = np.zeros((512, 512, 3), np.uint8)
+windowName = "config"
+cv.namedWindow(windowName)
+cv.namedWindow("Canny")
+cv.createTrackbar("HLow", windowName, 0, 179, emptyFunction)
+cv.createTrackbar("SLow", windowName, 0, 255, emptyFunction)
+cv.createTrackbar("VLow", windowName, 0, 255, emptyFunction)
 
-rho = 1  # distance resolution in pixels of the Hough grid
-theta = np.pi / 180  # angular resolution in radians of the Hough grid
-threshold = 15  # minimum number of votes (intersections in Hough grid cell)
-min_line_length = 50  # minimum number of pixels making up a line
-max_line_gap = 20  # maximum gap in pixels between connectable line segments
-line_image = np.copy(img)  # creating a blank to draw lines on
+cv.createTrackbar("HHigh", windowName, 0, 179, emptyFunction)
+cv.createTrackbar("SHigh", windowName, 0, 255, emptyFunction)
+cv.createTrackbar("VHigh", windowName, 0, 255, emptyFunction)
 
-# Run Hough on edge detected image
-# Output "lines" is an array containing endpoints of detected line segments
-lines = cv2.HoughLinesP(edges, rho, theta, threshold, np.array([]),
-                    min_line_length, max_line_gap)
+cv.createTrackbar("CannyLow", "Canny", 0, 255, emptyFunction)
+cv.createTrackbar("CannyHigh", "Canny", 0, 255, emptyFunction)
 
-for line in lines:
-    for x1,y1,x2,y2 in line:
-        cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),5)
+while True:
+    HLow = cv.getTrackbarPos("HLow", windowName)
+    SLow = cv.getTrackbarPos("SLow", windowName)
+    VLow = cv.getTrackbarPos("VLow", windowName)
+
+    HHigh = cv.getTrackbarPos("HHigh", windowName)
+    SHigh = cv.getTrackbarPos("SHigh", windowName)
+    VHigh = cv.getTrackbarPos("VHigh", windowName)
+
+    CannyLow = cv.getTrackbarPos("CannyLow", "Canny")
+    CannyHigh = cv.getTrackbarPos("CannyHigh", "Canny")
+
+    #LowImageBlue[:] = [HLow, SLow, VLow]
+    LowImageBlue[:] = [100, 125, 125]
+    LowImageBlue = cv.cvtColor(LowImageBlue, cv.COLOR_HSV2BGR)
+
+    #HighImageBlue[:] = [HHigh, SHigh, VHigh]
+    HighImageBlue[:] = [115, 255, 255]
+    HighImageBlue = cv.cvtColor(HighImageBlue, cv.COLOR_HSV2BGR)
+
+    if cv.waitKey(1) == 27:
+        cv.imshow("LowImageBlue", LowImageBlue)
+        cv.imshow("HighImageBlue", HighImageBlue)
+
+        #LowerBlue = np.array([HLow, SLow, VLow])
+        #UpperBlue = np.array([HHigh, SHigh, VHigh])
+        LowerBlue = np.array([100, 125, 125])
+        UpperBlue = np.array([115, 255, 255])
+
+        mask = cv.inRange(hsv, LowerBlue, UpperBlue)
+        grey = cv.bitwise_and(gray, gray, mask = mask)
+
+        BlurGrey = cv.GaussianBlur(grey,(5, 5),0)
+
+        edges = cv.Canny(BlurGrey, CannyLow, CannyHigh)
+        cv.imshow("edges", edges)
+
+        rho = 1  # distance resolution in pixels of the Hough grid
+        theta = np.pi / 180  # angular resolution in radians of the Hough grid
+        threshold = 15  # minimum number of votes (intersections in Hough grid cell)
+        min_line_length = 400  # minimum number of pixels making up a line
+        max_line_gap = 20  # maximum gap in pixels between connectable line segments
+        line_image = np.copy(img)  # creating a blank to draw lines on
+
+        # Run Hough on edge detected image
+        # Output "lines" is an array containing endpoints of detected line segments
+        lines = cv.HoughLinesP(edges, rho, theta, threshold, np.array([]),
+                            min_line_length, max_line_gap)
+
+        for line in lines:
+            for x1,y1,x2,y2 in line:
+                cv.line(line_image,(x1,y1),(x2,y2),(255,0,0),5)
 
 
-# Draw the lines on the  image
-lines_edges = cv2.addWeighted(img, 0.8, line_image, 1, 0)
-cv2.imshow("img", line_image)
-cv2.waitKey(0)
+        # Draw the lines on the  image
+        lines_edges = cv.addWeighted(img, 0.8, line_image, 1, 0)
+        cv.imshow("img", line_image)
